43ff3d389ad2996882c464bcccae4c265d48a4fd
Chris Bull <chris@chrisbull.com>
2017-07-24 01:47:31 -0700

    Fix lint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,15 +14,6 @@ module.exports = {
   ],
   rules: {
     'comma-dangle': [2, 'always-multiline'],
-    'object-curly-spacing': [2, 'always'],
-    'react/forbid-component-props': [0],
-    'react/jsx-max-props-per-line': [2, { maximum: 3 }],
-    'react/no-set-state': [0],
-    'react/jsx-sort-props': [1],
-    'react/require-optimization': [1],
-    'react/jsx-no-bind': [1, { allowArrowFunctions: true }],
-    'react/require-default-props': [0],
-    'react/no-multi-comp': [2, { ignoreStateless: true }],
     'import/no-named-as-default-member': [0],
     'prettier/prettier': [
       'error',
